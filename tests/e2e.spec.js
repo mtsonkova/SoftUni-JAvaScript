7d325220e64c7631ff371368513a89e4c72a3046
mtsonkova <mtsonkova950@gmail.com>
2024-07-04 22:36:16 +0300

    In progress

--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -5,6 +5,7 @@ const host = 'https://rahulshettyacademy.com/client';
 const email = 'samgreen@qa.com';
 const password = 'Qa_Password1';
 const expectedProductsTitles = ['ZARA COAT 3', 'ADIDAS ORIGINAL', 'IPHONE 13 PRO'];
+const expectedProduct = 'ZARA COAT 3';
 
 test('Purchase one product', async ({ page }) => {
     await page.goto(host);
@@ -20,11 +21,27 @@ test('Purchase one product', async ({ page }) => {
     const products = await page.locator('.card-body');
 
     const titles = await page.locator('.card-body b').allTextContents();
+    let size = await products.count();
+
+    for(let i = 0; i < size; ++i){
+        if(await products.nth(i).locator('b').textContent() === expectedProduct) {
+            await products.nth(i).locator('text=Add To Cart').click();
+            break;
+        }
+    }
+
+    await page.pause();
     
    
 })
 
+
+/*
+
 test('Purchase all products', async ({ page }) => {
+
+    const products = await page.locator('.card-body');
+
     await page.goto(host);
 
     //Login page form
@@ -35,8 +52,7 @@ test('Purchase all products', async ({ page }) => {
 
     //add product to shoping cart
 
-    const products = await page.locator('.card-body');
-
+    
     const titles = await page.locator('.card-body b').allTextContents();
     
    let size = await products.count();
@@ -69,3 +85,5 @@ test('Purchase all products', async ({ page }) => {
    
    
 });
+
+*/
