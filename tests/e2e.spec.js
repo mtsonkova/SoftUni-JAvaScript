31a80f213c709de7a5628c0d483be11bab6447ad
mtsonkova <mtsonkova950@gmail.com>
2024-07-06 01:13:56 +0300

    In progress

--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -23,14 +23,14 @@ test('Purchase one product', async ({ page }) => {
     const titles = await page.locator('.card-body b').allTextContents();
     let size = await products.count();
 
-    for(let i = 0; i < size; ++i){
-        if(await products.nth(i).locator('b').textContent() === expectedProduct) {
+    for (let i = 0; i < size; ++i) {
+        if (await products.nth(i).locator('b').textContent() === expectedProduct) {
             await products.nth(i).locator('text=Add To Cart').click();
             break;
         }
     }
 
-    await page.locator('[routerlink*="cart"]').click(); 
+    await page.locator('[routerlink*="cart"]').click();
 
     page.locator('div li').first().waitFor();
     const isElementPresentInCart = await page.locator('h3:has-text("ZARA COAT 3")').isVisible();
@@ -41,24 +41,54 @@ test('Purchase one product', async ({ page }) => {
 
     //ToDo
     //select payment method and fill credit card details
+
+    //Personal info
+    let personalInfo = await page.locator('.form__cc');
+
+    await personalInfo.locator('.text-validated').fill('5555 5555 5555 5555');
+
+    await personalInfo.locator('.form__cc select').first().selectOption('05');
+    await personalInfo.locator('.form__cc select').last().selectOption('15');
+
+    //CVV CODE
+    await page.locator('.form__cc .input').nth(1).fill('123');
+
+    //Name on card
+    await page.locator('.form__cc .input').nth(2).fill('Samantha Green');
+
+    //Coupon
+    await page.locator('.form__cc .input').nth(3).fill('RahulShettyAcademy');
+    //Apply Coupon btn
+    await page.click('.form__cc [type="submit"]');
+
+
+    //Shipping information
+    let userInfo = await page.locator('.user__name');
+    let userMail = userInfo.locator('.text-validated').textContent();
+//email
+expect(userMail).toEqual(email);
+    
+
+// click on place order btn
+
     await page.locator('[placeholder*="Country"]').pressSequentially('ind');
     const dropdown = await page.locator('.ta-results');
     await dropdown.waitFor();
 
     const numOfButtons = await dropdown.locator('button').count();
 
-    for(let i = 0; i < numOfButtons; i++) {
-        if(await dropdown.locator('button').nth(i).textContent() === ' Indonesia') {
+    for (let i = 0; i < numOfButtons; i++) {
+        if (await dropdown.locator('button').nth(i).textContent() === ' Indonesia') {
             await dropdown.locator('button').nth(i).click();
             break;
         }
     }
 
     //how to take screenshots on the entire page in playwright
-    await page.screenshot({path: 'screenshot.png'});
+    await page.screenshot({ path: 'screenshot.png' });
 
     // how to take screenshot of particular element only
-    await page.locator('displayed-text').screenshot({path: 'scrrenshot.png'});
+    await page.locator('displayed-text').screenshot({ path: 'scrrenshot.png' });
 })
 
 
