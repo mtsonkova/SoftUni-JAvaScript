2e6c74138d871d5222f725ab24fb2dd04a2c1e5d
mtsonkova <mtsonkova950@gmail.com>
2024-07-13 00:20:49 +0300

    In progress

--- playwright.config.js
+++ playwright.config.js
@@ -17,11 +17,12 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 1,
+  //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: 'html',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,9 +31,11 @@ module.exports = defineConfig({
     browserName: 'chromium',
     headless: true, // false -> will open the browser
     screenshot : 'off',
-    trace: 'retain-on-failure' //on, off, retain-on-failure
-
+    trace: 'retain-on-failure', //on, off, retain-on-failure
+  
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+
+
    
   },
 
--- tests/clientAppPageObject.spec.js
+++ tests/clientAppPageObject.spec.js
@@ -3,55 +3,59 @@ const { test, expect } = require('@playwright/test');
 const { POManager } = require('../pageobjects/POManager');
 const dataSet = JSON.parse(JSON.stringify(require("../testdata/clientAppPageObjects.json")));
 
+dataSet.forEach((data, index) => {
 
-const host = dataSet.host;
-const email = dataSet.email;
-const password = dataSet.password;
-const productName = dataSet.productName;
-const text = dataSet.text;
+    const host = data.host;
+    const email = data.email;
+    const password = data.password;
+    const productName = data.productName;
+    const text = data.text;
 
-test('Purchase one product', async ({ page }) => {
-    const poManager = new POManager(page);
-    const loginPage = poManager.getLoginPage();
-    await loginPage.goToUrl(host);
-    await loginPage.validLogin(email, password);
-    await page.waitForLoadState('networkidle');
+    //test.describe.configure({mode: 'serial'})
+    test(`Purchase one product ${index + 1} : ${data.id}`, async ({ page }) => {
+        const poManager = new POManager(page);
+        const loginPage = poManager.getLoginPage();
+        await loginPage.goToUrl(host);
+        await loginPage.validLogin(email, password);
+        await page.waitForLoadState('networkidle');
 
-    //add product to shoping cart
-    const dashboardPage = poManager.getDashboardPage();
-    await dashboardPage.addProductToCart(productName);
-    await dashboardPage.clickOnCartBtn();
+        //add product to shoping cart
+        const dashboardPage = poManager.getDashboardPage();
+        await dashboardPage.addProductToCart(productName);
+        await dashboardPage.clickOnCartBtn();
 
 
-    const cartPage = poManager.getCartPage();
+        const cartPage = poManager.getCartPage();
 
-    const isElementPresentInCart = await cartPage.isProductVisibleInCart(productName);
+        const isElementPresentInCart = await cartPage.isProductVisibleInCart(productName);
 
-    await expect(isElementPresentInCart).toBeTruthy();
-    await cartPage.clickCheckoutBtn();
+        await expect(isElementPresentInCart).toBeTruthy();
+        await cartPage.clickCheckoutBtn();
 
-    const checkoutPage = poManager.getCheckoutPage();
-    await checkoutPage.fillCheckoutPageWithValidData(dataSet.creditCardNumber, dataSet.month, dataSet.day, dataSet.cvvCode, dataSet.cardOwnerName, dataSet.country); 
-     await checkoutPage.clickPlaceOrderBtn();
+        const checkoutPage = poManager.getCheckoutPage();
+        await checkoutPage.fillCheckoutPageWithValidData(data.creditCardNumber, data.month, data.day, data.cvvCode, data.cardOwnerName, data.country);
+        await checkoutPage.clickPlaceOrderBtn();
 
-    const orderConfirmationPage = poManager.getOrderConfirmationPage()
-    let msg = await orderConfirmationPage.getMsgText();
+        const orderConfirmationPage = poManager.getOrderConfirmationPage()
+        let msg = await orderConfirmationPage.getMsgText();
 
-    await expect(msg).toEqual(text);
+        await expect(msg).toEqual(text);
 
-    let orderId = await orderConfirmationPage.getOrderId();
-    await orderConfirmationPage.clickOnOrdersBtn();
-   
-    let ordersPage = poManager.getOrdersPage();
-    await ordersPage.clickOnViewOrderBtn(orderId);
+        let orderId = await orderConfirmationPage.getOrderId();
+        await orderConfirmationPage.clickOnOrdersBtn();
+        await page.waitForLoadState('networkidle');
 
-    let orderSummaryPage = poManager.getOrderSummaryPage();
+        let ordersPage = poManager.getOrdersPage();
+        await ordersPage.clickOnViewOrderBtn(orderId);
 
-    let title = await orderSummaryPage.getOrderSummaryHeader();
-   
-    await expect(title).toEqual(' order summary ');
+        let orderSummaryPage = poManager.getOrderSummaryPage();
 
-    let orderIdSummary = await orderSummaryPage.getOrderSummaryId();
-    await expect(orderIdSummary).toEqual(orderId);
+        let title = await orderSummaryPage.getOrderSummaryHeader();
 
+        await expect(title).toEqual(' order summary ');
+
+        let orderIdSummary = await orderSummaryPage.getOrderSummaryId();
+        await expect(orderIdSummary).toEqual(orderId);
+
+    });
 });
